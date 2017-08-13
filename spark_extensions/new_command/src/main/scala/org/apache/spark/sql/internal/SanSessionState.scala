0782c736e6e12e2f209e747a66ab322d0b362eca
Sandeep Joshi <sandeep.joshi@dcengines.net>
2017-08-13 17:13:18 +0530

    Added DDLStrategy to SanSessionState

--- spark_extensions/new_command/src/main/scala/org/apache/spark/sql/internal/SanSessionState.scala
+++ spark_extensions/new_command/src/main/scala/org/apache/spark/sql/internal/SanSessionState.scala
@@ -3,6 +3,7 @@ package org.apache.spark.sql.internal
 import org.apache.spark.sql.SanSession
 import org.apache.spark.sql.parser.SanSparkSqlParser
 import org.apache.spark.sql.catalyst.parser.ParserInterface
+import org.apache.spark.sql.execution.command.DDLStrategy
 
 /**
   * Created by sandeep on 4/8/17.
@@ -11,6 +12,9 @@ class SanSessionState(sanSession: SanSession)
   extends SessionState(sanSession) {
 
   self =>
+
+  experimentalMethods.extraStrategies =
+    Seq(new DDLStrategy(sanSession))
 
   override lazy val sqlParser: ParserInterface = new SanSparkSqlParser(conf, sanSession)
 
